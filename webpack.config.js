8e50c152c387eaa2598e0628d1039d6f8d740ff2
linabaris <lina.fakhrutdinova@gmail.com>
2023-02-27 16:34:30 +0300

    create fireplace shop layout

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+    devServer:{
+        open:true,
+        hot:true,
+        port: 8000,
+    }
+}
+
+module.exports = ({develop}) => ({
+    mode: develop ? 'development' : 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: './styles/main.css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+    })],
+    module : {
+        rules: [
+            {
+                test : /\.(sa|sc|c)ss$/,
+                use : [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                          postcssOptions: {
+                            plugins: [
+                              [
+                                "postcss-preset-env",
+                                {
+                                  // Options
+                                },
+                              ],
+                            ],
+                          },
+                        },
+                    },
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
+                type: 'asset/inline'
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader'
+            }
+        ],
+    },
+    ...devServer(develop),
+})
